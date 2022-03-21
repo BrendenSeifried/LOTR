7590d1e1da845af7030894271f5b736ba960a713
BrendenSeifried <brendenseifried@gmail.com>
2022-03-21 12:05:37 -0700

    took out main in app.js added books section which loaded to page but no results trying to figure out how to set up Books.js in views now

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useEffect, useState } from 'react/cjs/react.production.min';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Main from './views/Main';
+// import Main from './views/Main';
+import Books from './views/Books/Books';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { fetchBooks } from './services/books';
 
 function App() {
   const [books, setBooks] = useState([]);
@@ -10,16 +12,23 @@ function App() {
   const [films, setFilms] = useState([]);
 
   useEffect(()=> {
-    
-  });
+    const everyItem = async () => {
+      const everyBook = await fetchBooks();
+      setBooks(everyBook);
+    };
+    everyItem();
+  }, []);
 
 
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route exact path='/'>
+          {/* <Route exact path='/'>
             <Main />
+          </Route> */}
+          <Route path = '/books'>
+            <Books books={books}/>
           </Route>
         </Switch>
       </div>
