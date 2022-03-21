9daeaaa2bb3a28e6b536b0e03af5a69b56fd1cad
BrendenSeifried <brendenseifried@gmail.com>
2022-03-21 11:52:07 -0700

    jumped the gun adde da bunch of .js files but chaving issues importing main in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useEffect, useState } from 'react/cjs/react.production.min';
 import './App.css';
+import Main from './views/Main';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  const [books, setBooks] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  const [films, setFilms] = useState([]);
+
+  useEffect(()=> {
+    
+  });
+
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path='/'>
+            <Main />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/views/Main/Main.js
@@ -0,0 +1,8 @@
+// import React, { useEffect, useState } from 'react';
+import React from 'react';
+
+export default function Main() {
+  return (
+    <div>Main</div>
+  );
+}
